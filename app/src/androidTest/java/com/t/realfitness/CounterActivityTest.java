1f4048c37037962f5706338ab59b83ab12929ed2
khant95 <khantzawphyo2595@gmail.com>
2021-02-21 08:31:19 +0500

    Testing

---
+++ app/src/androidTest/java/com/t/realfitness/CounterActivityTest.java
@@ -0,0 +1,12 @@
+package com.t.realfitness;
+
+import junit.framework.TestCase;
+
+public class CounterActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testOnPause() {
+    }
+}
---
+++ app/src/androidTest/java/com/t/realfitness/ExcerciseActivityTest.java
@@ -0,0 +1,18 @@
+package com.t.realfitness;
+
+import junit.framework.TestCase;
+
+public class ExcerciseActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testOnClick() {
+    }
+
+    public void testOnOptionsItemSelected() {
+    }
+
+    public void testOnNavigationItemSelected() {
+    }
+}
